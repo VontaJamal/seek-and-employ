cc58d80c273a20181c3f77f27087ea98415529d2
Vonta Johnson <devontajohnsonit@gmail.com>
2022-02-07 10:52:11 -0500

    add details to search results page

--- src/components/Dev/Dev.jsx
+++ src/components/Dev/Dev.jsx
@@ -1,12 +1,34 @@
 export default function Dev({ dev }) {
+  if (!dev) return null
+
+  const { url, login: name, followers, following, location, bio, avatar } = dev
   return (
-    <div className="card m-2 cursor-pointer border-2 border-gray-400 rounded-lg hover:shadow-md hover:border-purple-500 transform hover:-translate-y-1 transition-all duration-200">
-      <div className="m-3">
-        <h2 className="text-lg mb-2">-{dev.login}</h2>
-        <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200">
-          {dev.login}
-        </p>
-      </div>
-    </div>
+    <a href={url}>
+      <figure className="card m-2 cursor-pointer border-2 border-gray-400 rounded-lg hover:shadow-md hover:border-purple-500 transform hover:-translate-y-1 transition-all duration-200 h-full">
+        <div className="m-3">
+          <div className="flex justify-center items-center">
+            <img className="rounded-full py-2 w-32 h-25" src={avatar} alt={`${name}'s avatar`} />
+            <h2 className="text-lg mb-2 text-center p-2 font-bold">-{name}</h2>
+          </div>
+
+          {location && (
+            <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200 text-center">
+              🏠{location}
+            </p>
+          )}
+          <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200 px-4 m-4">
+            {bio}
+          </p>
+          <div className="flex justify-around">
+            <p className="font-light font-mono text-base text-gray-700 hover:text-gray-900 transition-all duration-200">
+              <span className="text-purple-500">{followers}</span> followers
+            </p>
+            <p className="font-light font-mono text-base text-gray-700 hover:text-gray-900 transition-all duration-200">
+              <span className="text-purple-500">{following}</span> following
+            </p>
+          </div>
+        </div>
+      </figure>
+    </a>
   )
 }
--- src/components/DevList/DevList.jsx
+++ src/components/DevList/DevList.jsx
@@ -1,11 +1,22 @@
-import { useDevSearchResults } from '../../hooks/useDevSearchResults'
+import { useEffect, useState } from 'react'
+import { fetchDevDetails } from '../../services/github'
 import Dev from '../Dev/Dev'
 
 export default function DevList({ devList = [] }) {
+  const [devDetails, setDevDetails] = useState([])
+
+  useEffect(() => {
+    const getDevDetails = async () => {
+      const detailData = await fetchDevDetails(devList)
+      setDevDetails(detailData)
+    }
+    getDevDetails()
+  }, [devList])
+
   return (
     <div className="container mt-4 mx-auto">
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {devList?.map((dev) => {
+        {devDetails?.map((dev) => {
           return (
             <li key={dev.id}>
               <Dev dev={dev} />
